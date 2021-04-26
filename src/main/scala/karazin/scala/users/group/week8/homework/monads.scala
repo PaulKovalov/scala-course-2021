3684bf1b39d99e2dd82234874fc03a363e8e4759
paul <soapmactavish799@gmail.com>
2021-04-26 13:11:51 +0300

    Start implementing hw-8

--- src/main/scala/karazin/scala/users/group/week8/homework/monads.scala
+++ src/main/scala/karazin/scala/users/group/week8/homework/monads.scala
@@ -12,10 +12,53 @@ package karazin.scala.users.group.week8.homework
       * Monad[ErrorOr] for a F[_], to be able to use List
  */
 
-import  karazin.scala.users.group.week2.and.three.quarters.homework.adt._
+import karazin.scala.users.group.week2.and.three.quarters.homework.adt._
 
 object monads:
 
-  trait Monad[F[_]]
+//  trait Monad[F[_]]:
+//    def apply[A](x: A): F[A]
+//    
+//    extension [A, B](x: F[A])
+//      def flatMap(f: A => F[B]): F[B]
+//
+//  end Monad
+//  
+//  given listMonad[A]: Monad[List] with
+//    def apply[A](x: A): List[A] =
+//      List(x)
+//
+//    extension [A, B](x: List[A])
+//      def flatMap(f: A => List[B]): List[B] =
+//        x.flatMap(f)
+//
+//
+//  given Monad[ErrorOr] with
+//    def apply[A](x: A): ErrorOr[A] =
+//      ErrorOr.apply(x)
+//    extension [A, B](x: ErrorOr[A])  
+//      def flatMap(f: A => ErrorOr[B]): ErrorOr[B] =
+//        x.flatMap(f)
+//  
+//  given Monad[ErrorOr[F[_]]] with
+//    def apply[A](x: A): ErrorOr[F[A]] = ???
+  trait Monad[F[_]]:
+
+    /** The unit value for a monad */
+    def pure[A](x: A): F[A]
+
+    extension [A, B](x: F[A])
+    /** The fundamental composition operation */
+      def flatMap(f: A => F[B]): F[B]
+
+  end Monad
+
+  given listMonad[A]: Monad[List] with
+    def pure[A](x: A): List[A] =
+      List(x)
+    extension [A, B](xs: List[A])
+      def flatMap(f: A => List[B]): List[B] =
+        xs.flatMap(f) // rely on the existing `flatMap` method of `List`
+
 
 end monads
