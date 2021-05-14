980177ebffa632585561e74e5ba80292b1613ba6
paul <soapmactavish799@gmail.com>
2021-05-14 13:22:18 +0300

    Redo hw-9

--- src/main/scala/karazin/scala/users/group/week9/homework/functors.scala
+++ src/main/scala/karazin/scala/users/group/week9/homework/functors.scala
@@ -14,44 +14,12 @@ object functors:
       * product of
     over Future[List[Try[Option[Int]]]]
    */
-  type LTOI = List[Try[Option[Int]]]
+  type FLTOI = Future[List[Try[Option[Int]]]]
 
-  private def listOp(v: Future[LTOI], op: (Int, Int) => Int, reduce: (Int, Int) => Int, neutral: Int): Future[Int] = {
+  def sum(l: FLTOI, n: Int): FLTOI =
+    Functor[Future].compose[List].compose[Try].compose[Option].map(l)((x: Int) => x + n)
 
-    def applyOp(elToAdd: Try[Option[Int]], l: LTOI): LTOI =
-      elToAdd match
-        case Success(Some(v)) => Functor[List].compose[Try].compose[Option].map(l)(x => op(x, v))
-        case _ => throw Exception("Future failed or Option had None")
-
-    def foldList(l: LTOI, initialList: LTOI): LTOI =
-      l match {
-        case Nil => initialList
-        case head :: tail => {
-          val addedList = foldList(tail, initialList)
-          applyOp(head, addedList)
-        }
-      }
-    v map { l =>
-      foldList(l, l).lift(0) match {
-        case Some(Success(Some(v))) => {
-          l.lift(0) match {
-            case Some(Success(Some(v2))) => reduce(v, v2)
-            case _ => neutral
-          }
-        }
-        case _ => neutral
-      }
-    }
-  }
-
-  def sum(l: Future[LTOI]): Future[Int] =
-    listOp(l, (x, y) => x + y, (x, y) => x - y, 0)
-
-  def mul(l: Future[LTOI]): Future[Int] =
-    listOp(l, (x, y) => x * y, (x, y) => {
-      x / (y match {
-        case 0 => 1
-        case _ => y
-      })}, 1)
+  def product(l: FLTOI, n: Int): FLTOI =
+    Functor[Future].compose[List].compose[Try].compose[Option].map(l)((x: Int) => x * n)
 
 end functors
--- src/test/scala/karazin/scala/users/group/week9/homework/FunctorsSuite.scala
+++ src/test/scala/karazin/scala/users/group/week9/homework/FunctorsSuite.scala
@@ -7,33 +7,42 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.postfixOps
-import scala.util.Try
-import scala.util.Random
-
+import scala.util.{Random, Success, Try}
 import cats.Functor
 import cats.implicits._
-
 import munit.ScalaCheckSuite
+
 import org.scalacheck.Prop._
 import org.scalacheck.Gen
 import org.scalacheck.Arbitrary
-
 
 
 class FunctorsSuite extends ScalaCheckSuite:
 
   property("test sum works") {
     forAll { (l: List[Int]) =>
-      val v: Future[LTOI] = Future.successful(l.map(el => Some(el)).map(el => Try(el)))
-      val sumValue = Await.result(sum(v), 2 seconds)
-      l.sum == sumValue
+      val list = l.map(el => Some(el)).map(el => Try(el))
+      val v: FLTOI = Future.successful(list)
+      val n = Random.between(1, 5)
+      val summed = Await.result(sum(v, n), 2 seconds)
+      summed.zip(list) foreach { tuple =>
+        tuple match
+          case (Success(Some(v1)), Success(Some(v2))) => assert(v1 == v2 + n)
+          case _ => fail("Future failed or Option had None")
+      }
     }
   }
 
   property("test mul works") {
-    forAll(Gen.listOfN(Random.between(0, 10), Arbitrary.arbitrary[Int].suchThat(x => x < 10 && x > -10))) { l =>
-      val v: Future[LTOI] = Future.successful(l.map(el => Some(el)).map(el => Try(el)))
-      val mulValue = Await.result(mul(v), 2 seconds)
-      mulValue == l.product
+    forAll(Gen.listOf(Arbitrary.arbitrary[Int].suchThat(x => x < 10 && x > -10))) { l =>
+      val list = l.map(el => Some(el)).map(el => Try(el))
+      val v: FLTOI = Future.successful(list)
+      val n = Random.between(1, 5)
+      val multiplied = Await.result(product(v, n), 2 seconds)
+      multiplied.zip(list) foreach { tuple =>
+        tuple match
+          case (Success(Some(v1)), Success(Some(v2))) => assert(v1 == v2 * n)
+          case _ => fail("Future failed or Option had None")
+      }
     }
   }
