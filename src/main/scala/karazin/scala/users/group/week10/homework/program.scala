50acba19c015accf46441003ac1413b0dae03ade
paul <soapmactavish799@gmail.com>
2021-05-20 11:10:35 +0300

    Homework 10 implementation

--- src/main/scala/karazin/scala/users/group/week10/homework/program.scala
+++ src/main/scala/karazin/scala/users/group/week10/homework/program.scala
@@ -1,6 +1,9 @@
 package karazin.scala.users.group.week10.homework
+import cats.Applicative
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
-object program:
+object program extends App:
 
   /*
     Provide a meaningful example of Applicative usage.
@@ -8,9 +11,16 @@ object program:
    */
 
   object applicatives:
-  
-    ???
-    
+    val getAccountABalance =
+      Future.successful(Some(100.0))
+
+    val getAccountBBalance =
+      Future.successful(Some(200.0))
+
+    def getTotalBalance(balance1: Future[Option[Double]], balance2: Future[Option[Double]]): Future[Option[Double]] =
+      Applicative[Future].compose[Option].map2(balance1, balance2)(_ + _)
+
+    getTotalBalance(getAccountABalance, getAccountBBalance)
   end applicatives
 
 end program
